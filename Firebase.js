8bc40c44f1246fb7994e1b4e2433ec357a646ce0
ShamasadBuilt <shamsada@builtvisor.com>
2024-02-28 13:53:47 +0530

    Firebase Configuration

---
+++ Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyC9Ps99vedrd9dNscomAEKBtiBSN3eI6Wo",
+  authDomain: "chat-app-573f6.firebaseapp.com",
+  projectId: "chat-app-573f6",
+  storageBucket: "chat-app-573f6.appspot.com",
+  messagingSenderId: "1038456229581",
+  appId: "1:1038456229581:web:73989ae345f49152f62384",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
